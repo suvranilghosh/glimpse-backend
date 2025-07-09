b166ed2c68c598cf69615262056ab01c8271e4d3
Suvranil Ghosh <sg1239@scarletmail.rutgers.edu>
2025-07-09 13:36:11 -0400

    updated prisma client import

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
